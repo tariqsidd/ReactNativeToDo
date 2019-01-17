58d347b72f31a71f238f8c2fdea39ea1b8b6cdae
tariqsidd <tariqsidd@ymail.com>
2019-01-17 20:42:50 +0500

    fix minor issues

--- src/components/add/Add.js
+++ src/components/add/Add.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, StyleSheet, Text, TextInput, Image, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux'
 import AddStyle from "./AddStyle";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { addTodo } from "../../actions/todos";
 
 class Add extends Component {
@@ -21,6 +22,7 @@ class Add extends Component {
     render() {
         return (
             <View style={AddStyle.mainCont}>
+                <KeyboardAwareScrollView>
                 <View>
                     <TextInput placeholder="What do you need to do"
                                placeholderTextColor="#e5e5e5"
@@ -37,12 +39,10 @@ class Add extends Component {
                                returnKeyType='go'
                     />
                 </View>
-                <View style={AddStyle.timeInputCont}>
-
-                </View>
                 <TouchableOpacity onPress={() => this.addTodo()} style={AddStyle.btn}>
                     <Text style={AddStyle.btnText}>Add</Text>
                 </TouchableOpacity>
+                </KeyboardAwareScrollView>
             </View>
         );
     }
